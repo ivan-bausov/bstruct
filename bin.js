c7b6708768327f2a30a40e15708a599e20168585
Ivan Bausov <ivan.bausov@gmail.com>
2015-07-21 20:58:59 +0300

    source file watcher fixes

--- bin.js
+++ bin.js
@@ -3,7 +3,7 @@
  * Created by Ivan on 20/07/15.
  */
 var fs = require('fs'),
-    Compiler = require('./compiler.t.js');
+    Compiler = require('../compiler.t.js');
 
 function readContent(path) {
     return fs.readFileSync(path, {
@@ -38,6 +38,11 @@ function processQueue() {
             fs.writeFileSync(current_folder_path + target_file.replace(/\.ctdl$/, '.html'), html);
             fs.writeFileSync(current_folder_path + target_file.replace(/\.ctdl$/, '.scss'), scss);
 
+            console.log('Change processed:');
+            console.log('Files written:');
+            console.log('\t' + current_folder_path + target_file.replace(/\.ctdl$/, '.html'));
+            console.log('\t' + current_folder_path + target_file.replace(/\.ctdl$/, '.scss'));
+
         } catch (e) {
             console.log(e);
         }
@@ -53,7 +58,12 @@ if (process.argv[2] === '--watch') {
     } else {
         target_file = process.argv[3];
 
-        fs.watch(current_folder_path + target_file, function () {
+        console.log('This script will poll for changes: ' + current_folder_path + target_file + '. Press Ctrl+C to exit polling.');
+
+        fs.watchFile(current_folder_path + target_file, {
+            interval: 2000
+        }, function () {
+            console.log('\n\nChange detected on ' + current_folder_path + target_file);
             queue.push({});
             processQueue();
         });
